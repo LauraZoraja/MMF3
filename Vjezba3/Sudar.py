70bd0bdb3ad64f9dcdbd8417fbda066edc6f1bcb
unknown <lzoraja@pmfst.hr>
2025-10-21 17:31:53 +0200

    provjerit tocnost i dodat txt output

--- Vjezba3/Sudar.py
+++ Vjezba3/Sudar.py
@@ -23,7 +23,7 @@ def df(t):
     return df1(t) - df2(t)
 
 # Bisection
-def bisekcija(a, b, tol=1e-10, Nmax=200):
+def bisekcija(a, b, tol=10**(-6), Nmax=200):
     fa = f(a)
     fb = f(b)
     if fa * fb > 0:
@@ -31,7 +31,7 @@ def bisekcija(a, b, tol=1e-10, Nmax=200):
     for i in range(1, Nmax+1):
         c = 0.5 * (a + b)
         fc = f(c)
-        print("{:2d} {:.6f}".format(i, c))  # print iteration number and c as in the PDF
+        print("{:2d} {:.10f}".format(i, c))  # print iteration number and c as in the PDF
         if abs(fc) < tol or (b - a)*0.5 < tol:
             return c, i
         if fa * fc < 0:
@@ -41,7 +41,7 @@ def bisekcija(a, b, tol=1e-10, Nmax=200):
     return 0.5*(a+b), Nmax
 
 # Newton-Raphson
-def newton(x0, tol=1e-12, Nmax=100):
+def newton(x0, tol=10**(-10), Nmax=100):
     x = x0
     for i in range(1, Nmax + 1):
         fx = f(x)
@@ -61,5 +61,8 @@ t_newt, iter_newt = newton(2.1, tol=1e-12)
 y_at_t_bis = y01 + A*math.cos(B*t_bis)
 y_at_t_newt = y01 + A*math.cos(B*t_newt)
 
-print("Bisekcija: t = {:.12f}, iteracije = {}, y = {:.12f}".format(t_bis, iter_bis, y_at_t_bis))
-print("Newton:    t = {:.12f}, iteracije = {}, y = {:.12f}".format(t_newt, iter_newt, y_at_t_newt))
+diff_bis = abs(f1(t_bis) - f2(t_bis))
+diff_newt = abs(f1(t_newt) - f2(t_newt))
+
+print("Bisekcija: t = {:.12f}, iteracije = {}, y = {:.12f}".format(t_bis, iter_bis, diff_bis))
+print("Newton:    t = {:.12f}, iteracije = {}, y = {:.12f}".format(t_newt, iter_newt, diff_newt))
